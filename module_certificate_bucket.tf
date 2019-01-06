e0f4165e45d8727563008a9609ba5dcb2053a82d
Sion Smith <sion.smith@gmail.com>
2019-01-06 18:59:04 +0000

    replaced relative path with absolute

--- module_certificate_bucket.tf
+++ module_certificate_bucket.tf
@@ -1,5 +1,5 @@
 module "certificates_bucket" {
-  source                  = "../s3"
+  source                  = "git::ssh://git@github.com/osodevops/aws-terraform-module-s3.git"
   s3_bucket_name          = "certificates-${data.aws_caller_identity.current.account_id}"
   s3_bucket_policy        = "${data.template_file.policy.rendered}"
   s3_bucket_force_destroy = "${var.s3_bucket_force_destroy}"
