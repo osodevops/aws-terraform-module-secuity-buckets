b7cecc3a673b0c0e41b53b506c1fcf5ec6c377dd
Sion Smith <sion.smith@gmail.com>
2019-11-29 06:16:56 +0000

    refactored code for terraform 12.

--- data.template_file.policy.tf
+++ data.template_file.policy.tf
@@ -2,7 +2,7 @@ data "template_file" "policy" {
   template = "${file("${path.module}/files/s3_certificate_policy.json")}"
 
   vars {
-    s3_bucket_name = "certificates-${data.aws_caller_identity.current.account_id}"
-    account_id     = "${data.aws_caller_identity.current.account_id}"
+    s3_bucket_name = local.certificate_bucket_name
+    account_id     = data.aws_caller_identity.current.account_id
   }
 }
